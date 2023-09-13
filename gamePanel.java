4a56aa49880f74e0ddef0e623c9c750043a2a58d
FS-Baz <s443011919@st.uqu.edu.sa>
2023-09-13 13:21:41 +0300

    A few details

--- gamePanel.java
+++ gamePanel.java
@@ -1,5 +1,7 @@
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.StrokeBorder;
+
 import java.awt.event.*;
 
 
@@ -9,9 +11,9 @@ public class gamePanel extends JPanel implements ActionListener {
     final int HEIGHT =400;
     final int UNITS_SIZE =1;
     final int PLAYER_UNIT =(WIDTH*HEIGHT)-UNITS_SIZE;
-    JPanel image = new JPanel();
+    JPanel panel = new JPanel();
     JLabel score = new JLabel();
-    ImageIcon Star ;
+    ImageIcon ball = new ImageIcon("Star.png");
     Action upAction;
     Action downAction;
     Action player2Action;
@@ -24,7 +26,7 @@ public class gamePanel extends JPanel implements ActionListener {
     int y =200;
     int red_score;
     int blue_score;
-    int moveVelocity=6;
+    int moveVelocity=7;
     int[] x1 ={20,20,20,20,20};
     int[] y1 ={50,60,70,80,90};
     int[] x2 ={750,750,750,750,750};
@@ -34,7 +36,7 @@ public class gamePanel extends JPanel implements ActionListener {
         
         this.setFocusable(true);
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-        this.setBackground(Color.pink);
+        this.setBackground(new Color(152,210,94));
         upAction = new UpAction();
         downAction = new DownAction();
         this.getInputMap().put(KeyStroke.getKeyStroke("UP"),"upAction");
@@ -44,22 +46,27 @@ public class gamePanel extends JPanel implements ActionListener {
         score.setBounds(200, 100, 50, 15);
         score.setBackground(Color.DARK_GRAY);
         this.add(score);
-        Star =  new ImageIcon("Star.png");
-                
-        timer = new Timer(15,this);
+        timer = new Timer(10,this);
         timer.start();
     }
 
     public void paint(Graphics g){
         super.paint(g);
-        Graphics2D g2D = (Graphics2D) g ;
-        g2D.drawImage(Star.getImage(), x, y, null);
-        // g2D.setStroke(new BasicStroke(13)); 
+        Graphics2D g2D = (Graphics2D) g;
+
+        g2D.setColor(Color.white);
+        g2D.setStroke(new BasicStroke(5));
+        g2D.drawLine(WIDTH/2, 0, WIDTH/2, HEIGHT);
+
+
+        g2D.drawImage(ball.getImage(), x, y, this);
         g2D.setFont(new Font("Tempus Sans ITC", Font.BOLD, 20));
         g2D.setColor(Color.BLUE);
-        g2D.drawString(blue_score+" ", 360, 20);
+        g2D.drawString(blue_score+"", 375, 20);
         g2D.setColor(Color.red);
-        g2D.drawString(red_score+" ", 400, 20);
+        g2D.drawString(red_score+"", 415, 20);
+        
+        
         for (int i =0;i<bodyParts;i++){
             // g2D.setColor(Color.GREEN);
             // g2D.drawLine(x1[0]+10,y1[0]-5,x1[4]+10,y1[4]+25);
@@ -70,21 +77,19 @@ public class gamePanel extends JPanel implements ActionListener {
             g2D.setColor(Color.red);
             g2D.drawRect((int)x2[i],(int) y2[i], 10, 20);
             g2D.fillRect((int)x2[i],(int) y2[i], 10, 20);
+
             }
-    
-
-
         }
 
     void Palyer2Move() {
-        System.out.println("Hi i'm player 2");    
-        if (y2[0]>y){
+        // System.out.println("Hi i'm player 2");    
+        if (y2[0]>y-75){
         for (int i = bodyParts-1; i >= 0; i--){ 
             y2[i]=y2[i]-2;
             repaint();}
         }
         
-        if (y2[4]<y){
+        if (y2[4]<y+75){
         for (int i = bodyParts-1; i >= 0; i--) {
             y2[i]=y2[i]+2;
             repaint();}
@@ -93,14 +98,14 @@ public class gamePanel extends JPanel implements ActionListener {
         
 // x2[0],y2[0]-5,x2[4],y2[4]+25
     void paddle(){
-        if (x>=WIDTH-Star.getIconWidth()||x<0 || x==34&&y>=y1[0]-15&&y<=y1[0]+35){
+        if (x>=WIDTH-ball.getIconWidth()||x<0 || x==34&&y>=y1[0]-50&&y<=y1[0]+50){
         xVelocity=xVelocity*-1;
         }
 
-         if (x>=WIDTH-Star.getIconWidth()||x>800 || x>=702&y>-y2[0]-5&y<=y2[0]+28){
+         if (x>=WIDTH-ball.getIconWidth()||x>800 || x>=702&y>=y2[0]-50&y<=y2[0]+50){
         xVelocity=xVelocity*-1;
         }
-
+        repaint();
     }
 
 
@@ -108,7 +113,7 @@ public class gamePanel extends JPanel implements ActionListener {
     public void actionPerformed(ActionEvent e) {
 
 
-        if (y>=HEIGHT-Star.getIconHeight() || y<0){
+        if (y>=HEIGHT-ball.getIconHeight() || y<0){
             yVelocity=yVelocity*-1;   
         }
 
@@ -123,14 +128,14 @@ public class gamePanel extends JPanel implements ActionListener {
     void checkColiision(){
         if (x<=-1){
             red_score++;
-            System.out.println("Goal for red !" + y1[0]);
+            // System.out.println("Goal for red !" + y1[0]);
             y=200;
             x=400;
         }
         
         if (x>=750){
             blue_score++;
-            System.out.println("Goal for blue ! ");
+            // System.out.println("Goal for blue ! ");
             y=200;
             x=400;
         }
@@ -145,8 +150,9 @@ public class UpAction extends AbstractAction{
         for (int i = bodyParts-1; i >= 0; i--){ 
                 y1[i]=y1[i]-moveVelocity;
                 repaint();}
-        System.out.println("going up! " + y1[0]);}
-        
+        // System.out.println("going up! " + y1[0]);
+        }
+
     }
 }
 
@@ -158,28 +164,10 @@ public class DownAction extends AbstractAction{
         for (int i = bodyParts-1; i >= 0; i--) {
                 y1[i]=y1[i]+moveVelocity;
                 repaint();}
-        System.out.println("going Down! "+ y1[4]);  
+        // System.out.println("going Down! "+ y1[4]);  
                  
         
     }
-    
-
- 
-
-
 }
-
-
-
-
-    
-
-
-
-
-
-
-
-
 
 }
